8caaebe38e46ab2bd91d6dcb13e975b19007cfdf
ForsakenAginor <juriys2022@mail.ru>
2025-03-10 19:06:29 +0400

    add save system

---
+++ Assets/Source/Scripts/SaveSystem/ISaveLoadService.cs
@@ -0,0 +1,9 @@
+﻿namespace SaveSystem
+{
+    public interface ISaveLoadService
+    {
+        public string GetSavedInfo();
+
+        public void SaveInfo(string value);
+    }
+}
---
+++ Assets/Source/Scripts/SaveSystem/PlayerPrefsSaveLoadService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+namespace SaveSystem
+{
+    public class PlayerPrefsSaveLoadService : ISaveLoadService
+    {
+        private readonly string _playerPrefsKey = nameof(_playerPrefsKey);
+
+        public PlayerPrefsSaveLoadService(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+                throw new ArgumentNullException(nameof(key));
+
+            _playerPrefsKey = key;
+        }
+
+        public string GetSavedInfo()
+        {
+            if (PlayerPrefs.HasKey(_playerPrefsKey) == false)
+                return null;
+
+            return PlayerPrefs.GetString(_playerPrefsKey);
+        }
+
+        public void SaveInfo(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException(nameof(value));
+
+            PlayerPrefs.SetString(_playerPrefsKey, value);
+        }
+    }
+}
